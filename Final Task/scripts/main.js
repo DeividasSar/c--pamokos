f6f9fab52fa3c93791fc839261f06e8437b606af
DeividasSar <deividas.sarocka@gmail.com>
2022-10-24 18:00:07 +0300

    Fixed login and note editing

--- Final Task/scripts/main.js
+++ Final Task/scripts/main.js
@@ -2,27 +2,24 @@ const form = document.querySelector("#new-task-form");
 const inputTask = document.querySelector("#new-task-input");
 const taskType = document.querySelector("#task-type");
 const taskDate = document.querySelector("#task-date");
-
 const userNamePost = document.querySelector("#user-name");
 const userEmailPost = document.querySelector("#user-email");
 const userLogOut = document.querySelector("#logout");
-
 const listElement = document.querySelector("#tasks");
 
 userNamePost.innerHTML = sessionStorage.getItem("userName");
 userEmailPost.innerHTML = sessionStorage.getItem("userEmail");
 const userId = sessionStorage.getItem("userId");
-const type = sessionStorage.getItem("taskType");
-const content = sessionStorage.getItem("taskTask");
-const endDate = sessionStorage.getItem("taskEndDate");
-const id = sessionStorage.getItem("taskID")
 
 var alltasksRawData = tasksRawDataApi();
 
+if(sessionStorage.getItem("toastr") == "Success")
+{
+    toastr.success('LoggedIn!');
+}
+
 userLogOut.addEventListener("click", () => {
-    sessionStorage.removeItem('userId');
-    sessionStorage.removeItem('userName');
-    sessionStorage.removeItem('userEmail');
+    sessionStorage.clear();
     alert("Good bye!")
     window.location.href ="../pages/login.html";
 });
@@ -57,48 +54,44 @@ function tasksRawDataApi(){
     .then((response) => response.json())
     .then((result) => {
         if (result.error) {
-
         }
         else{
             const container = document.querySelector('#tasks');
             for (let i=0; i < result.length; i++) {
                 if(result[i].userId == userId) {
 //TASK CREATE
+                    const taskId = document.createElement("div");
+                    const taskType = document.createElement("div");
+                    const taskContent = document.createElement("div");
+                    const taskEndDate = document.createElement("div");
 
+                    taskId.innerHTML =`${result[i].id}`;
+                    taskType.innerHTML =`${result[i].type}`;
+                    taskContent.innerHTML = `${result[i].content}`;
+                    taskEndDate.innerHTML = `${result[i].endDate.slice(0,10)}`;
 
-                    const task = document.createElement("div");
-
-                    sessionStorage.setItem('taskID', result[i].id);
-                    sessionStorage.setItem('taskType', result[i].type);     
-                    sessionStorage.setItem('taskTask', result[i].content);
-                    sessionStorage.setItem('taskEndDate', result[i].endDate);
-
-                    task.innerHTML= `
-                   <b> Task ID: ${result[i].id} </b><br>
-                    Where to do task: ${result[i].type} <br> 
-                    Task: ${result[i].content} <br> 
-                    End Date: ${result[i].endDate.slice(0,10)}`
-                    container.append(task);
-                    task.classList.add("#tasks");
-            
-
+                    container.insertAdjacentText('beforeEnd', 'Task ID: ') + container.append(taskId);
+                    container.insertAdjacentText('beforeEnd', 'Where to do task:') + container.append(taskType);
+                    container.insertAdjacentText('beforeEnd', 'Task:') + container.append(taskContent);
+                    container.insertAdjacentText('beforeEnd', 'End Date:') + container.append(taskEndDate);
 
 //EDIT AND DELETE BUTTONS
                     const taskAction = document.createElement("div");
-
                     const taskDelete = document.createElement("button");
                     taskDelete.classList.add("delete");
                     taskDelete.innerHTML = "Delete";
 
                     const taskEdit = document.createElement("button");
-                    const todoBtn = document.createElement("button");    //TEST
 
                     taskEdit.classList.add("edit");
                     taskEdit.innerHTML = "Edit";
 
                     taskAction.appendChild(taskEdit);
                     taskAction.appendChild(taskDelete);
-                    task.appendChild(taskAction);
+                    taskId.appendChild(taskAction);
+                    taskType.appendChild(taskAction);
+                    taskContent.appendChild(taskAction);
+                    taskEndDate.appendChild(taskAction);
                     listElement.appendChild(taskAction);
 //DELETE
                     taskDelete.addEventListener("click", () => {
@@ -107,18 +100,21 @@ function tasksRawDataApi(){
                                                 });
                                                 window.location.href = "../pages/main.html";
                         });
- 
 //EDIT
                     taskEdit.addEventListener("click", () => {
                         if (taskEdit.innerText.toLocaleLowerCase() == "edit") {
-                            task.contentEditable = true;
-                            task.style.backgroundColor = "#dddbdb";
-                            task.focus();
+
+                            taskId.contentEditable = true; 
+                            taskId.style.backgroundColor = "#dddbdb"; 
+                            taskType.contentEditable = true; 
+                            taskType.style.backgroundColor = "#dddbdb";
+                            taskContent.contentEditable = true; 
+                            taskContent.style.backgroundColor = "#dddbdb";
+                            taskEndDate.contentEditable = true; 
+                            taskEndDate.style.backgroundColor = "#dddbdb"; 
                             taskEdit.innerHTML = "Save";
 
                             taskEdit.addEventListener("click", () => {
-
-                                task.innerText
                                 fetch(`https://localhost:7171/api/ToDo/${result[i].id}`, {
                                     method: "PUT",
                                     headers: {
@@ -126,16 +122,19 @@ function tasksRawDataApi(){
                                     },
                                     body: JSON.stringify({
                                         userId,
-                                        type,
-                                        content,
-                                        endDate,
-                                        userId,
-                                        id,
+                                        type : taskType.innerText,
+                                        content : taskContent.innerText,
+                                        endDate : taskEndDate.innerText,
+                                        id : taskId.innerText
                                     }),
                                 });
+                                window.location.href = "../pages/main.html";
                             });
                         }else {
-                            task.setAttribute("readonly", "readonly");
+                            taskId.setAttribute("readonly", "readonly");
+                            taskType.setAttribute("readonly", "readonly");
+                            taskContent.setAttribute("readonly", "readonly");
+                            taskEndDate.setAttribute("readonly", "readonly");
                             taskEdit.innerHTML = "Edit";
                     }
 })
